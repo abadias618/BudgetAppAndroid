57cdd80da086d7552fda3b8cc99162ed20537acc
abadias618 <46508823+abadias618@users.noreply.github.com>
2020-03-14 15:20:41 -0600

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/byui/budgetappandroid/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/byui/budgetappandroid/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.e.budgetappandroid", appContext.getPackageName());
+        assertEquals("com.byui.budgetappandroid", appContext.getPackageName());
     }
 }
