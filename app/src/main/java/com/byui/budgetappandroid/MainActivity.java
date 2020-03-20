6239a7218c72984ac5844ec64b18794ef26bfd47
fnhenriques <nandoje@hotmail.com>
2020-03-20 00:27:47 -0300

    first screen

--- app/src/main/java/com/byui/budgetappandroid/MainActivity.java
+++ app/src/main/java/com/byui/budgetappandroid/MainActivity.java
@@ -3,6 +3,9 @@ package com.byui.budgetappandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner mySpinner = (Spinner) findViewById(R.id.spinner1);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.categories));
+        // specify adapter to dropdown list
+                myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                //This line allow the adapter to show the adapter inside the spinner.
+                mySpinner.setAdapter(myAdapter);
+
+
+
     }
 
     //david push test
