4642e8fb6c6d37ad54bc9626e357eac8ce35d284
rbowery762 <bow18018@byui.edu>
2020-03-28 15:13:45 -0500

    Added Settings buttons (The one on the Sign in page is temporary), added as much API compatability as possible without Firebase access

--- app/src/main/java/com/byui/budgetappandroid/MainActivity.java
+++ app/src/main/java/com/byui/budgetappandroid/MainActivity.java
@@ -1,10 +1,14 @@
 package com.byui.budgetappandroid;
 
-import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -13,20 +17,34 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_entdata);
+        setContentView(R.layout.activity_main);
 
         Spinner mySpinner = findViewById(R.id.spinner1);
         // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(MainActivity.this,
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<>(MainActivity.this,
                 android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.categories));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // specify adapter to dropdown list
                 myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                 //This line allow the adapter to show the adapter inside the spinner.
                 mySpinner.setAdapter(myAdapter);
 
+        final Button _settings = findViewById(R.id.settingsButton);
 
+
+        _settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                startActivity((new Intent(getApplicationContext(), Settings.class)));
+                finish();
+
+            }
+
+        });
 
     }
+
+
 
     //david push test
     //rachel push test 2.0
--- app/src/main/java/com/byui/budgetappandroid/Settings.java
+++ app/src/main/java/com/byui/budgetappandroid/Settings.java
@@ -11,64 +11,117 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
-//import org.json.simple.JSONObject;
 import android.view.View;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class Settings extends AppCompatActivity {
+public class Settings extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+
+    private String pickedCurrency;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        //Retrieve any input from the "currency" field
-        final EditText _currencyInput = findViewById(R.id.currencyInput);
-        final TextView _submitButton = findViewById(R.id.submit);
 
         //The user's login info
         FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
 
         //If the user's not logged in, go back to the login page. Otherwise, continue.
-        if (firebaseAuth.getCurrentUser() == null) {
+/*        if (firebaseAuth.getCurrentUser() == null) {
             startActivity((new Intent(getApplicationContext(), Login.class)));
             finish();
         }
+*/
+        //Retrieve any input from the "currency" field
+        Spinner spinner = findViewById(R.id.currencyInput);
+        ArrayAdapter<CharSequence> adapter =
+                ArrayAdapter.createFromResource(this, R.array.currencies, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        //Listen for when/if the user selects an item
+        spinner.setOnItemSelectedListener(this);
+
+
+        final TextView _submitButton = findViewById(R.id.submit);
+
 
         _submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view){
+                if(pickedCurrency != null) {
 
-                String currency = _currencyInput.getText().toString().trim();
+                    //save current currency from Firebase to a variable
+                    //save new currency in a variable (returned by onItemSelected)
+                    //call API
+                    //use API to load just the new currency's conversion rate
 
-                //Make sure the currency input field is not empty
-                if (TextUtils.isEmpty(currency)) {
-                    Toast.makeText(Settings.this, "User Created Successfully",
-                            Toast.LENGTH_SHORT).show();
                 }
 
-                //Call API
+                startActivity((new Intent(getApplicationContext(), MainActivity.class)));
+                finish();
+
             }
+
         });
-
-  //Cannot work until JSONObject problem is resolved. I (Rachel), am working on it
-        /*
-        public static void main(String[] args) {
-            JSONObject obj = new JSONObject();
-
-            obj.put("name", "foo");
-            obj.put("num", new Integer(100));
-            obj.put("balance", new Double(1000.21));
-            obj.put("is_vip", new Boolean(true));
-
-            System.out.print(obj);
-        }
-*/
 
     }
 
+    public void currencyConversion(String oldCurrency, String newCurrency) throws IOException {
+        //Setting up the URL for the API. NOTE: The "amount" value is not yet added to the URL string
+        URL url = new URL("http://data.fixer.io/api/convert?access_key=edbef9eb81e730c20186f2be117dec47&from="
+                + oldCurrency + "&to=" + newCurrency + "&amount=");
+
+        //Loop through each cost value stored in database
+        //Download each cost and concat to the end of the URL, replacing the original value
+        //
+
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        connection.setRequestMethod("GET");
+
+
+
+        Toast.makeText(Settings.this, "New",
+                Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String selection = ((TextView)view).getText().toString();
+        switch (selection) {
+            case "US Dollars":
+                pickedCurrency = "USD";
+                break;
+            case "Aus Dollars":
+                pickedCurrency = "AUD";
+                break;
+            default:
+                pickedCurrency = "EUR";
+        }
+        Toast.makeText(Settings.this, pickedCurrency,
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
